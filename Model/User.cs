3cc4544e4b0eba84ef8f103fd47391cc27c1a763
Guilherm Consentino <guiconsentino@hotmail.com>
2022-08-07 21:31:47 -0300

    meu objeto do banco de dados

---
+++ Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MysqlApi.Model
+{
+    public class User
+    {
+
+        [Required]
+        public int Age { get; set; }
+        
+        [Required]
+        public string? Name { get; set; }
+        
+        [Required]
+        public int Id { get; set; }
+
+
+    }
+}
