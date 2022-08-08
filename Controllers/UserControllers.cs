1bc44473ff1dff1fe2261590b6c0b37a8e21e1f8
Guilherm Consentino <guiconsentino@hotmail.com>
2022-08-07 21:38:49 -0300

    responsável pelo processamento das requisições e por gerar respostas

---
+++ Controllers/UserControllers.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MysqlApi.Model;
+using MysqlApi.Repository;
+
+namespace MysqlApi.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAll()
+        {
+            var users = _userRepository.GetAll();
+
+            if (users.Count() == 0)
+                return NoContent();
+
+            return Ok(users);
+        }
+
+        [HttpPost()]
+        public IActionResult Post(User newUser)
+        {
+            var response = _userRepository.InsertUser(newUser);
+
+            return Ok("number of lines affected "+response);
+        }
+
+        [HttpDelete()]
+
+        public IActionResult Delete(int id)
+        {
+            var response = _userRepository.DeleteUser(id);
+
+            return Ok("Users delete "+response);
+        }
+  
+    
+    }
+}
