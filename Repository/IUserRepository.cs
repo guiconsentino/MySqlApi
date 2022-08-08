70dc20a311a8e1b417042251a32211c88f9c402c
Guilherm Consentino <guiconsentino@hotmail.com>
2022-08-07 21:33:12 -0300

    interface e metodos

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using MysqlApi.Model;
+
+namespace MysqlApi.Repository
+{
+    public interface IUserRepository
+    {
+
+        IEnumerable<User> GetAll();
+        int InsertUser(User newUser);
+        int DeleteUser(int Id);
+
+    }
+
+}
+
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using MysqlApi.Model;
+
+namespace MysqlApi.Repository
+{
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            var sql = "SELECT * FROM user";
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                return connection.Query<User>(sql);
+                //usando dapper        
+            }
+
+        }
+
+        public int InsertUser(User newUser)
+        {
+            var sql = "INSERT INTO USER VALUES(@Id, @Name, @Age)";
+
+            User user = new User()
+            {
+                Id = newUser.Id,
+                Name = newUser.Name,
+                Age = newUser.Age,
+            };
+
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                return connection.Execute(sql, user);
+            }
+
+        }
+
+        public int DeleteUser(int id)
+        {
+            var sql = "DELETE FROM USER WHERE id = @Id";
+
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+
+                return connection.Execute(sql, new { id });
+            };
+
+        }
+    }
+}
+
